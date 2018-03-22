a4714d94657ba83bfd47fcab6ae9e01730276d13
harrydemo <harry.han@gmail.com>
2018-03-21 22:41:04 -0400

    added local  to remote

---
+++ taotao-portal/src/main/java/com/taotao/portal/controller/CartController.java
@@ -0,0 +1,61 @@
+package com.taotao.portal.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.portal.pojo.CartItem;
+import com.taotao.portal.service.CartService;
+
+/**
+ * 购物车Controller
+ * <p>Title: CartController</p>
+ * <p>Description: </p>
+ * <p>Company: www.itcast.com</p> 
+ * @author	入云龙
+ * @date	2015年9月15日下午3:12:59
+ * @version 1.0
+ */
+@Controller
+@RequestMapping("/cart")
+public class CartController {
+
+	@Autowired
+	private CartService cartService;
+	
+	@RequestMapping("/add/{itemId}")
+	public String addCartItem(@PathVariable Long itemId, 
+			@RequestParam(defaultValue="1")Integer num, 
+			HttpServletRequest request, HttpServletResponse response) {
+		TaotaoResult result = cartService.addCartItem(itemId, num, request, response);
+		return "redirect:/cart/success.html";
+	}
+	
+	@RequestMapping("/success")
+	public String showSuccess() {
+		return "cartSuccess";
+	}
+	
+	@RequestMapping("/cart")
+	public String showCart(HttpServletRequest request, HttpServletResponse response, Model model) {
+		List<CartItem> list = cartService.getCartItemList(request, response);
+		model.addAttribute("cartList", list);
+		return "cart";
+	}
+	
+	@RequestMapping("/delete/{itemId}")
+	public String deleteCartItem(@PathVariable Long itemId, HttpServletRequest request, HttpServletResponse response) {
+		cartService.deleteCartItem(itemId, request, response);
+		return "redirect:/cart/cart.html";
+	}
+	
+}
